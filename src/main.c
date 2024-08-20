5a137fa90b806183c7e0f3a79e1e1a8a489a4bdf
Brandon Retana <branrecha140356@gmail.com>
2024-08-20 00:18:40 -0600

    feat: add mutex test

--- src/main.c
+++ src/main.c
@@ -1,6 +1,43 @@
+#include<pthread.h>
 #include <stdio.h>
+#include<stdlib.h>
+#include<mutex.c>
+
+#define NUM_THREADS 5
+
+mutex_t mutex;
+
+void test(void *arg){
+    long thread_id = (long)arg;
+    printf("Hilo %ld intentando adquirir el mutex...\n", thread_id);
+    my_mutex_lock(&mutex);
+
+    // Sección crítica
+    printf("Hilo %ld en la sección crítica.\n", thread_id);
+    sleep(1);  // Simula trabajo en la sección crítica
+
+    printf("Hilo %ld liberando el mutex.\n", thread_id);
+    my_mutex_unlock(&mutex);
+
+    return NULL;    
+}
 
 int main() {
-    printf("Hola, Mundo!\n");
+    pthread_t threads[NUM_THREADS];
+
+    my_mutex_init(&mutex);
+
+    // Crear hilos
+    for (long i = 0; i < NUM_THREADS; i++) {
+        pthread_create(&threads[i], NULL, test, (void*)i);
+    }
+
+    // Esperar a que los hilos terminen
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    my_mutex_destroy(&mutex);
+
     return 0;
 }
--- src/mutex.c
+++ src/mutex.c
@@ -6,16 +6,16 @@ typedef struct{
     pthread_cond_t cond;
     pthread_mutex_t internal_mutex;
     int locked;
-} mutex;
+} mutex_t;
 
 
-void mutex_init(mutex *m){
+void mutex_init(mutex_t *m){
     pthread_cond_init(&m->cond, NULL);
     pthread_mutex_init(&m->internal_mutex, NULL);
     m->locked = 0;
 }
 
-void mutex_lock(mutex *m){
+void mutex_lock(mutex_t *m){
     pthread_mutex_lock(&m->internal_mutex);
     while (m->locked){
         pthread_cond_wait(&m->cond, &m->internal_mutex);
@@ -24,14 +24,14 @@ void mutex_lock(mutex *m){
     pthread_mutex_unlock(&m->internal_mutex);
 }
 
-void mutex_unlock(mutex *m){
+void mutex_unlock(mutex_t *m){
     pthread_mutex_lock(&m->internal_mutex);
     m->locked = 0;
     pthread_cond_signal(&m->cond);
     pthread_mutex_unlock(&m->internal_mutex);   
 }
 
-void mutex_destroy(mutex *m){
+void mutex_destroy(mutex_t *m){
     pthread_cond_destroy(&m->cond);
     pthread_mutex_destroy(&m->internal_mutex);
 }
