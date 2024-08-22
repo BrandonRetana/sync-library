ac2f3d6aee32b5f4c8d4ec1fa328f73b59e65396
Brandon Retana <branrecha140356@gmail.com>
2024-08-21 22:44:09 -0600

    feat: add barrier test

--- src/main.c
+++ src/main.c
@@ -4,17 +4,48 @@
 #include<stdlib.h>
 #include<../mutex/mutex.h>
 #include <../semaforo/semaforo.h>
+#include <../barrera/barrera.h>
+
+
+#define NUM_THREADS 5
+
+barrier barr;
+int shared_counter = 0;
+
+void *thread_func(void *arg) {
+    int id = *(int*)arg;
+    printf("Thread %d llegando a la primera barrera.\n", id);
+    barrier_wait(&barr);
+    printf("Thread %d pasando la primera barrera.\n", id);
+
+    // Incremento del contador sin mutex
+    shared_counter++;
+
+    printf("Thread %d llegando a la segunda barrera.\n", id);
+    barrier_wait(&barr);
+    printf("Thread %d pasando la segunda barrera.\n", id);
+
+    return NULL;
+}
 
 int main() {
-    semaphore sem;
-    semaphore_init(&sem, 1);
+    pthread_t threads[NUM_THREADS];
+    int thread_ids[NUM_THREADS];
 
-    printf("Probando el semáforo...\n");
-    semaphore_wait(&sem);
-    printf("Dentro de la sección crítica.\n");
-    semaphore_signal(&sem);
-    printf("Fuera de la sección crítica.\n");
+    barrier_init(&barr, NUM_THREADS);
 
-    semaphore_destroy(&sem);
+    for (int i = 0; i < NUM_THREADS; i++) {
+        thread_ids[i] = i;
+        pthread_create(&threads[i], NULL, thread_func, &thread_ids[i]);
+    }
+
+    for (int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    barrier_destroy(&barr);
+
+    printf("Valor final del contador compartido: %d\n", shared_counter);
+
     return 0;
 }
