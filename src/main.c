c77030bee31a48cfcd86e8100550e5a23b8920ed
Brandon Retana <branrecha140356@gmail.com>
2024-08-26 19:20:28 -0600

    feat: add test read write lock

--- src/main.c
+++ src/main.c
@@ -5,47 +5,67 @@
 #include<../mutex/mutex.h>
 #include <../semaforo/semaforo.h>
 #include <../barrera/barrera.h>
+#include <../read-write-lock/read-write.h>
 
+#define NUM_READERS 5
+#define NUM_WRITERS 2
+#define NUM_READS 10
+#define NUM_WRITES 5
 
-#define NUM_THREADS 5
+rwlock rwlock_t;
+int shared_data = 0;
 
-barrier barr;
-int shared_counter = 0;
+void *reader_function(void *arg) {
+    int id = *(int *)arg;
+    for (int i = 0; i < NUM_READS; i++) {
+        rwlock_acquire_read(&rwlock_t);
+        printf("Reader %d: Read shared_data as %d\n", id, shared_data);
+        rwlock_release_read(&rwlock_t);
+    }
+    return NULL;
+}
 
-void *thread_func(void *arg) {
-    int id = *(int*)arg;
-    printf("Thread %d llegando a la primera barrera.\n", id);
-    barrier_wait(&barr);
-    printf("Thread %d pasando la primera barrera.\n", id);
-
-    // Incremento del contador sin mutex
-    shared_counter++;
-
-    printf("Thread %d llegando a la segunda barrera.\n", id);
-    barrier_wait(&barr);
-    printf("Thread %d pasando la segunda barrera.\n", id);
-
+void *writer_function(void *arg) {
+    int id = *(int *)arg;
+    for (int i = 0; i < NUM_WRITES; i++) {
+        rwlock_acquire_write(&rwlock_t);
+        shared_data++;
+        printf("Writer %d: Incremented shared_data to %d\n", id, shared_data);
+        rwlock_release_write(&rwlock_t);
+    }
     return NULL;
 }
 
 int main() {
-    pthread_t threads[NUM_THREADS];
-    int thread_ids[NUM_THREADS];
+    pthread_t readers[NUM_READERS], writers[NUM_WRITERS];
+    int reader_ids[NUM_READERS], writer_ids[NUM_WRITERS];
 
-    barrier_init(&barr, NUM_THREADS);
+    rwlock_init(&rwlock_t);
 
-    for (int i = 0; i < NUM_THREADS; i++) {
-        thread_ids[i] = i;
-        pthread_create(&threads[i], NULL, thread_func, &thread_ids[i]);
+    // Create reader threads
+    for (int i = 0; i < NUM_READERS; i++) {
+        reader_ids[i] = i;
+        pthread_create(&readers[i], NULL, reader_function, &reader_ids[i]);
     }
 
-    for (int i = 0; i < NUM_THREADS; i++) {
-        pthread_join(threads[i], NULL);
+    // Create writer threads
+    for (int i = 0; i < NUM_WRITERS; i++) {
+        writer_ids[i] = i;
+        pthread_create(&writers[i], NULL, writer_function, &writer_ids[i]);
     }
 
-    barrier_destroy(&barr);
+    // Wait for all threads to complete
+    for (int i = 0; i < NUM_READERS; i++) {
+        pthread_join(readers[i], NULL);
+    }
+    for (int i = 0; i < NUM_WRITERS; i++) {
+        pthread_join(writers[i], NULL);
+    }
 
-    printf("Valor final del contador compartido: %d\n", shared_counter);
+    rwlock_destroy(&rwlock_t);
+
+    printf("Final shared_data value: %d\n", shared_data);
 
     return 0;
 }
+
